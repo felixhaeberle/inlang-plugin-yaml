1a468625be1e9215b866b979a3a8c0c23939a454
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2023-06-05 13:54:25 +0000

    Add language support #3

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -21,3 +21,47 @@ test("inlang's config validation should pass", async () => {
   }
   expect(isOk).toBe(true);
 });
+
+test("inlang's config validation should pass with new language", async () => {
+  // create random string as new language
+  const randomLanguageName = Math.random().toString(36).substring(7);
+
+  const env = await mockEnvironment({
+    copyDirectory: {
+      fs: fs,
+      paths: ["./dist", "./example"],
+    },
+  });
+
+  const module = await import("../inlang.config.js");
+
+  const config = await setupConfig({ module, env });
+
+  const resources = await config.readResources({ config });
+
+  // add new language
+  await config.writeResources({config, resources: [
+    ...resources,
+    {
+      type: "Resource",
+      languageTag: {
+        type: "LanguageTag",
+        name: randomLanguageName,
+      },
+      body: [],
+    }]
+  })
+
+  const newConfig = await setupConfig({ module, env });
+  
+  // check if new language is added
+  expect(newConfig.languages).toContain(randomLanguageName);
+
+  const [isOk, error] = await testConfig({ config: newConfig });
+  if (error) {
+    throw error;
+  }
+
+  expect(isOk).toBe(true);
+});
+
